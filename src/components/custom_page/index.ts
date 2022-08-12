45ae7b9fc00303d87a152def7f54f9cc45359f95
citrono-lemon <citrono.lemon@gmail.com>
2022-08-12 12:27:49 +0900

    Recreate #1

--- src/components/custom_page/index.ts
+++ src/components/custom_page/index.ts
@@ -1,8 +1,1 @@
-//export { default as Footer } from './Footer'
-//export { default as Navbar } from './Navbar'
 export { default as Top } from './top/Top'
-export { default as News } from './news/News'
-export { default as Article } from './news/Article'
-export { default as Works } from './works/Works'
-
-// export { default as Game } from './game/Game'
--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Head from 'next/head'
-import Footer from './Footer'
-import Navbar from './Navbar/Navbar'
 import { url } from 'inspector'
 
 type Props = {
